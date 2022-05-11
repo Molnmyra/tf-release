7e86f601feb7b108279745daf218f29fb2e1ec9d
Daniel-Redeploy <84455776+Daniel-Redeploy@users.noreply.github.com>
2022-05-11 12:23:30 +0200

    add resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,9 @@
-resource "azurerm_resource_group" "example" {
-  name     = "tf-release-${var.azure_environment}"
-  location = var.azure_location
+locals {
+  prefix = substr(sha1("${data.azurerm_client_config.current.tenant_id}/${data.azurerm_client_config.current.subscription_id}"), 0, 5)
+}
+
+resource "azurerm_private_dns_zone" "private_dns" {
+  name                = "${var.azure_environment}.azure.local"
+  resource_group_name = "${local.prefix}-rg-${var.environment}-environment"
+
 }
