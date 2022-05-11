6444b4bccca3e3798d39e67bf2bf7bce35c7216b
Daniel-Redeploy <84455776+Daniel-Redeploy@users.noreply.github.com>
2022-05-11 22:19:12 +0200

    add env tag

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,7 @@ resource "azurerm_private_dns_zone" "private_dns" {
   resource_group_name = "${local.prefix}-rg-${var.azure_environment}-environment"
 
   tags = {
-    "commit" = var.github_sha
+    "commit"      = var.github_sha
+    "environment" = var.azure_environment
   }
 }
